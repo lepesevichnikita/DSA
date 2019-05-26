a97382eb1ad2c78e134faf3c80eed6e499995195
Nikita Lepesevich <lepesevich.nikita@yandex.com>
2019-05-26 17:32:46 +0300

    import SchnorrSchemeValidatorModel
    Signed-off-by: Nikita Lepesevich <lepesevich.nikita@yandex.com>

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -5,4 +5,5 @@ from .dsa_keys_writer_model import DSAKeysWriterModel
 from .dsa_sign_checker_model import DSASignCheckerModel
 from .dsa_signer_model import DSASignerModel
 from .schnorr_scheme_client_model import SchnorrSchemeClientModel
+from .schnorr_scheme_keygen_model import SchnorrSchemeKeygenModel
 from .schnorr_scheme_validator_model import SchnorrSchemeValidatorModel

a075d8b746ae0e1afa1c0bae9912c078ab7863e4
Nikita Lepesevich <lepesevich.nikita@yandex.com>
2019-05-26 17:28:03 +0300

    remove checking on not null public_key_path and not null private_key_path in constructor remove keys property rewrite public and private key path properties in new style
    Signed-off-by: Nikita Lepesevich <lepesevich.nikita@yandex.com>

--- src/dsa/dsa_keys_reader.py
+++ src/dsa/dsa_keys_reader.py
@@ -3,37 +3,27 @@ from .dsa_public_key import DSAPublicKey
 
 
 class DSAKeysReader(DSABase):
-    def __init__(self, public_key_path: str = None,
-                 private_key_path: str = None):
+    def __init__(self, public_key_path: str = "",
+                 private_key_path: str = ""):
         super().__init__()
-        if public_key_path is not None:
-            self._public_key_path = public_key_path
-        if private_key_path is not None:
-            self._private_key_path = private_key_path
-
-    def _get_keys(self) -> list:
-        return self.keys_container.keys
-
-    def _set_keys(self, keys: list):
-        self.keys_container.keys = keys
-
-    keys = property(_get_keys, _set_keys)
-
-    def _get_public_key_path(self) -> str:
-        return self._public_key_path
-
-    def _set_public_key_path(self, public_key_path: str):
         self._public_key_path = public_key_path
-
-    public_key_path = property(_get_public_key_path, _set_public_key_path)
-
-    def _get_private_key_path(self):
-        return self._private_key_path
-
-    def _set_private_key_path(self, private_key_path: str):
         self._private_key_path = private_key_path
 
-    private_key_path = property(_get_private_key_path, _set_private_key_path)
+    @property
+    def public_key_path(self) -> str:
+        return self._public_key_path
+
+    @public_key_path.setter
+    def public_key_path(self, public_key_path: str):
+        self._public_key_path = public_key_path
+
+    @property
+    def private_key_path(self) -> str:
+        return self._private_key_path
+
+    @private_key_path.setter
+    def private_key_path(self, private_key_path: str):
+        self._private_key_path = private_key_path
 
     def read_public_key(self):
         with open(self._public_key_path, 'r') as file:
